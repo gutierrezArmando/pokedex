ef5544ef6b086643a70852a24afdb601ffd6c4c0
Armando <37322555+gutierrezArmando@users.noreply.github.com>
2025-10-16 10:09:06 -0700

    commit init
    Crea páginas list-pokemons y detail-pokemo, se eliminar la página home y se actualiza app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'list-pokemons',
     pathMatch: 'full',
   },
+  {
+    path: 'list-pokemons',
+    loadComponent: () => import('./list-pokemons/list-pokemons.page').then( m => m.ListPokemonsPage)
+  },
+  {
+    path: 'detail-pokemon',
+    loadComponent: () => import('./detail-pokemon/detail-pokemon.page').then( m => m.DetailPokemonPage)
+  },
 ];
+
